30980f859d6dc45db137c3d7af83ad8be75efb50
wflying000 <wf123580@163.com>
2026-08-10 20:57:41 +0800

    sort

--- coding_interview/lcr137_regular_expression_matching.py
+++ coding_interview/lcr137_regular_expression_matching.py
@@ -7,6 +7,7 @@ https://leetcode.cn/problems/regular-expression-matching/
 class Solution:
     def articleMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
+        # dp[i][j] 的意义定义为p[0 ... j-1]是否能匹配s[0 ... i - 1]
         dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
         dp[0][0] = True
 
@@ -20,10 +21,15 @@ class Solution:
             for j in range(1, n + 1):
                 sc = s[i - 1]
                 pc = p[j - 1]
+
+                # p[j - 1]为'*'时，p[j - 2]可以使用0次和至少使用1次
+                # 如果p[j - 2]使用零次等价与判断p[0 .. j-3]是否可以与s[0 .. i - 1]匹配
+                # 如果p[j - 2]至少使用1次，需要保证p[j-2]能匹配s[i-1]即p[j-2]==s[i-1]或p[j-2]=='.', 然后判断p[0..j-1]能否匹配s[0..i-2]
                 if pc == "*":
                     dp[i][j] = dp[i][j - 2]
                     if p[j - 2] == sc or p[j - 2] == '.' :
                         dp[i][j] = dp[i][j] or dp[i - 1][j]
+
                     
                 elif pc == sc or pc == '.':
                     dp[i][j] = dp[i - 1][j - 1]
---
+++ sort/sort.py
@@ -0,0 +1,155 @@
+
+import random
+
+def selection_sort(nums):
+    if not nums:
+        return
+    n = len(nums)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+        nums[i], nums[min_idx] = nums[min_idx], nums[i]
+
+
+def insertion_sort(nums):
+    if not nums:
+        return
+    n = len(nums)
+    for i in range(1, n):
+        j = i
+        while j > 0 and nums[j] < nums[j - 1]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            j -= 1
+
+
+def bubble_sort(nums):
+    if not nums:
+        return
+
+    n = len(nums)
+
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                swapped = True
+        if not swapped:
+            break
+                
+
+def merge_sort(nums):
+    if not nums:
+        return
+    
+    merge_sort_core(nums, 0, len(nums) - 1)
+
+def merge(nums, left, mid, right):
+    tmp = []
+    i, j = left, mid + 1
+    while i <= mid and j <= right:
+        if nums[i] <= nums[j]:
+            tmp.append(nums[i])
+            i += 1
+        else:
+            tmp.append(nums[j])
+            j += 1
+
+    if i <= mid:
+        tmp += nums[i : mid + 1]
+    elif j <= right:
+        tmp += nums[j : right + 1]
+
+    # n = right - left + 1
+    # for k in range(n):
+    #     nums[left + k] = tmp[k]
+    
+    nums[left : right + 1] = tmp
+
+def merge_sort_core(nums, left, right):
+    if left >= right:
+        return
+    mid = (right - left) // 2 + left
+    merge_sort_core(nums, left, mid)
+    merge_sort_core(nums, mid + 1, right)
+    merge(nums, left, mid, right)
+    
+
+def quick_sort(nums):
+    if not nums:
+        return
+    quick_sort_core(nums, 0, len(nums) - 1)
+
+def quick_sort_core(nums, left, right):
+    if left >= right:
+        return
+    
+    idx = partition(nums, left, right)
+    quick_sort_core(nums, left, idx - 1)
+    quick_sort_core(nums, idx + 1, right)
+
+def partition(nums, left, right):
+    pivot = nums[left]
+    i, j = left, right
+    while i < j:
+        while i < j and nums[j] > pivot:
+            j -= 1
+        while i < j and nums[i] < pivot:
+            i += 1
+        nums[i], nums[j] = nums[j], nums[i]
+
+    nums[i], nums[left] = nums[left], nums[i]
+
+    return i
+
+
+def test_selection_sort():
+    nums = [random.randint(0, 100) for _ in range(10)]
+    nums_sorted = sorted(nums)
+    selection_sort(nums)
+    assert nums == nums_sorted
+
+
+def test_insertion_sort():
+    nums = [random.randint(0, 100) for _ in range(10)]
+    nums_sorted = sorted(nums)
+    print(f"before sorted, nums: {nums}")
+    insertion_sort(nums)
+    print(f"after sorted, nums: {nums}")
+    assert nums == nums_sorted
+
+def test_bubble_sort():
+    nums = [random.randint(0, 100) for _ in range(10)]
+    nums_sorted = sorted(nums)
+    print(f"before sorted, nums: {nums}")
+    bubble_sort(nums)
+    print(f"after sorted, nums: {nums}")
+    assert nums == nums_sorted
+
+def test_merge_sort():
+    nums = [random.randint(0, 100) for _ in range(10)]
+    nums_sorted = sorted(nums)
+    print(f"before sorted, nums: {nums}")
+    merge_sort(nums)
+    print(f"after sorted, nums: {nums}")
+    assert nums == nums_sorted
+
+def test_quick_sort():
+    nums = [random.randint(0, 100) for _ in range(10)]
+    nums_sorted = sorted(nums)
+    print(f"before sorted, nums: {nums}")
+    quick_sort(nums)
+    print(f"after sorted, nums: {nums}")
+    assert nums == nums_sorted
+
+def main():
+    # test_selection_sort()
+    # test_insertion_sort()
+    # test_bubble_sort()
+    # test_merge_sort()
+    test_quick_sort()
+
+if __name__ == "__main__":
+    main()
